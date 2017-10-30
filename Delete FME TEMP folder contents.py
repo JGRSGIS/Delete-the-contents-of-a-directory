5e49846afde9e6d04b20bb990d053d3413880fa9
Jgillespie1990 <jgillespie1990@googlemail.com>
2017-10-30 16:15:51 +0000

    Add files via upload

---
+++ Delete FME TEMP folder contents.py
@@ -0,0 +1,14 @@
+#!/bin/python
+import os, shutil
+folder = 'D:\FME_TEMP'
+for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    except Exception as e:
+        print(e)
+		
+		
+## Talk to James G for set up
