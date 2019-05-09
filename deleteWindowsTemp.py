f5e74e7260cded7a77e05d230a3ac81739f77fb6
James Gillespie <11785794+JGRSGIS@users.noreply.github.com>
2019-05-09 11:59:41 +0100

    Further files to clear up disk space

---
+++ deleteWindowsTemp.py
@@ -0,0 +1,11 @@
+#!/bin/python
+import os, shutil
+folder = 'C:\\Windows\\Temp'
+for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    except Exception as e:
+    	print(e)
